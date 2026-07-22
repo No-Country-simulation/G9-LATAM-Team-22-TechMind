c3a278d572d8d537e1b9dea9b1e7d1caa4cb4da3
Mónica Rincón Gallardo Nava <monica.rincongallardo@gmail.com>
2026-07-22 11:34:41 -0600

    feat: configuracion inicial de Spring Boot y estructura base

---
+++ src/main/java/com/techmind/api/TechmindApiApplication.java
@@ -0,0 +1,13 @@
+package com.techmind.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechmindApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechmindApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techmind/api/TechmindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmind.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechmindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
